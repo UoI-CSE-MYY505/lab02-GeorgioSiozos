3d93a6c988df0c4716f9459e6247a28b6972ab99
GeorgioSiozos <cs05434@uoi.gr>
2024-10-20 16:46:59 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -11,6 +11,26 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+    add t1, zero, zero #index
+    add s0, zero, zero #Address / array[s0]
+    
+loop:
+    beq a1, zero, done 
+    slli s1, t1, 2 #OFFSET
+    add s1, s1, a0 #Address = Offset + base address / Address of array[i] more
+    # specifically address[t1]
+    lw t2, 0(s1) # t2 = array[i]
+    bne t2, a2, loop2
+    add s0, s1, zero 
+    #sw t2, 0(s0)
+     
+loop2:
+    addi t1, t1, 1
+    addi a1, a1, -1
+    j loop
+    
+             
+    
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
