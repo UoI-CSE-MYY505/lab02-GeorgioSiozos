49ac117e57ac065ec9ede2a37054c8cb559a894f
GeorgioSiozos <cs05434@uoi.gr>
2024-10-22 23:10:39 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -1,13 +1,13 @@
 
 .data
 
-array: .word 1, 0, 1, 12, 0, 1, 4
+array: .word 0, 1, 2, 4, 4, 5
 
 .text
 
     la a0, array
-    li a1, 7    # unsigned
-    li a2, 1
+    li a1, 6    # unsigned
+    li a2, 4
 prog:
 #-----------------------------
 # Write your code here!
@@ -19,7 +19,7 @@ loop:
     slli s1, t1, 2 #OFFSET
     add s1, s1, a0 #Address = Offset + base address / Address of array[i] more
     # specifically address[t1]
-    lw t2, 0(s1) # t2 = array[i]
+    lw t2, 0(s1) # t2 = array[t1]
     bne t2, a2, loop2
     add s0, s1, zero 
     #sw t2, 0(s0)
